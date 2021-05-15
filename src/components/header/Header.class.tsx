d8f3724023e0086bac529a4c06f4c184a4a8e9af
陳栄超 <48024543+chenmiaochao@users.noreply.github.com>
2021-05-15 17:53:36 +0900

    some thing wrong

--- src/components/header/Header.class.tsx
+++ src/components/header/Header.class.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import styles from "./Header.module.css";
 import logo from '../..//assets/logo.svg';
-import { Layout, Typography, Input, Menu, Button, Dropdown } from "antd"
-import { GlobalOutlined } from '@ant-design/icons'
-import { withRouter, RouteComponentProps } from "react-router-dom"
+import { Layout, Typography, Input, Menu, Button, Dropdown } from "antd";
+import { GlobalOutlined } from '@ant-design/icons';
+import { withRouter, RouteComponentProps } from "react-router-dom";
+import store from "../../redux/store";
+import { LanguageState } from "../../redux/languageReducer";
+
+interface State extends LanguageState {}
 
 class HeaderComponent extends React.Component<RouteComponentProps>{
+  
+  constructor(props) {
+    super(props);
+    const storeState = store.getState();
+    this.state = {
+      language: storeState.language,
+      languageList: storeState.languageList,
+    };
+  }
+
   render() {
     const { history } = this.props;
     return (
@@ -18,13 +32,14 @@ class HeaderComponent extends React.Component<RouteComponentProps>{
               style={{ marginLeft: 10 }}
               overlay={
                 <Menu>
-                  <Menu.Item>中文</Menu.Item>
-                  <Menu.Item>English</Menu.Item>
+                  {this.state.languageList.map((l) => {
+                    return <Menu.Item key={l.code}>{l.name}</Menu.Item>
+                  })}
                 </Menu>
               }
               icon={<GlobalOutlined />}
             >
-              语言
+              {this.state.language === "zh" ? "中文" : "English"};
             </Dropdown.Button>
             <Button.Group className={styles['button-gruop']}>
               <Button onClick={() => history.push("register")}>注册</Button>
--- src/redux/languageReducer.ts
+++ src/redux/languageReducer.ts
@@ -1,4 +1,4 @@
-interface LanguageState {
+export interface LanguageState {
     language: "en" | "zh";
     languageList: { name: string; code: string }[];
 }
@@ -7,9 +7,9 @@ const defaultState: LanguageState = {
     language: "zh",
     languageList: [
         {name:"中文", code:"zh"},
-        {name:"English", code:"en"}
+        {name:"English", code:"en"},
     ],
-}
+};
 
 export default (state = defaultState, action) => {
     return state;
